0cf680bfe80cd204ba73c2321da40174862b0655
Charles Muzonzini <charles@webgems.co.zw>
2020-04-01 19:06:31 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# An Ionic Google Maps Demo App
+
+### [please support us by purchasing awesome tech & merchendise from from our online store: www.codeswag.co.uk/shop](https://www.codeswag.co.uk/shop)
+
+This is the code repository for the Ionic Google Maps Demo App with custom markers.
+
+Use git to clone this repository to your computer, then using your commandline program run `npm install` while inside the project folder in order to install the dependencies.
+
+Run `ionic serve` to launch the app in your web browser.
+
+Run `ionic cordova run android` to launch the app in the Android emulator.
+
+Run `ionic cordova run ios` to launch the app in the iOS simulator.
+
+**Make sure you create your own Google Maps API Key, otherwise the app will not work! Add the api key to index.html as demonstrated in part 1 of the tutorial.**
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  infoWindows: any = [];
+  markers: any = [
+    {
+        title: "National Art Gallery",
+        latitude: "-17.824991",
+        longitude: "31.049295"
+    },
+    {
+        title: "West End Hospital",
+        latitude: "-17.820987",
+        longitude: "31.039682"
+    },
+    {
+        title: "Dominican Convent School",
+        latitude: "-17.822647",
+        longitude: "31.052042"
+    },
+    {
+        title: "Chop Chop Brazilian Steakhouse",
+        latitude: "-17.819460",
+        longitude: "31.053844"
+    },
+    {
+        title: "Canadian Embassy",
+        latitude: "-17.820972",
+        longitude: "31.043587"
+    }
+  ];
+
+  constructor() {}
+
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  addMarkersToMap(markers) {
+    for (let marker of markers) {
+      let position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      let mapMarker = new google.maps.Marker({
+        position: position,
+        title: marker.title,
+        latitude: marker.latitude,
+        longitude: marker.longitude
+      });
+
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker) {
+    let infoWindowContent = '<div id="content">' +
+                              '<h2 id="firstHeading" class"firstHeading">' + marker.title + '</h2>' +
+                              '<p>Latitude: ' + marker.latitude + '</p>' +
+                              '<p>Longitude: ' + marker.longitude + '</p>' +
+                            '</div>';
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    });
+
+    marker.addListener('click', () => {
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+    });
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows() {
+    for(let window of this.infoWindows) {
+      window.close();
+    }
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(-17.824858, 31.053028);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarkersToMap(this.markers);
+  }
+
+}
